7e67f507a69a5950673bfb51f6ac7a3b3d4f4fad
Isabella <isabellapierce93@gmail.com>
2023-01-30 18:12:26 -0700

    Added get function for single user in userController.

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -9,7 +9,16 @@ getUser(req, res) {
     .catch((err) => res.status(500).json(err));
 },
 // Get a user 
-
+getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+    .select('-__v')
+    .then((user) => 
+    !user
+        ? res.status(404).json({ message: 'No User with that id'})
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
 // Create a user
 
 // Delete a user
