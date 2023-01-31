01b80e1683de539bc25e13c4d1412d7dc6b0ecc8
Isabella <isabellapierce93@gmail.com>
2023-01-30 17:02:46 -0700

    Outlined user and thought controllers.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,15 @@
+const { User, Thought } = require('../models');
+
+// Get all thoughts
+
+// Get a thought
+
+// Create a thought
+
+// Delete a thought
+
+// Update a thought
+
+// Create reaction
+
+// Delete reaction
---
+++ controllers/usercontroller.js
@@ -0,0 +1,15 @@
+const { User, Thought } = require('mongoose');
+
+// Get all users
+
+// Get a user 
+
+// Create a user
+
+// Delete a user
+
+// Update a user
+
+// Add a friend
+
+// Delete a friend
